4b15cc2fae0b2aea227a3a6a723131776a32a5e5
Cédric Bonhomme <cedric@cedricbonhomme.org>
2018-01-15 15:26:35 +0100

    set the default TTL to 60.

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -194,7 +194,7 @@ return array(
     ],
 
     'monarc' => array(
-        'ttl' => 20, // timeout
+        'ttl' => 60, // timeout
         'salt' => '', // salt privé pour chiffrement pwd
     ),
 );
