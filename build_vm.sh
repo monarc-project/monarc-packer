cd5c4385fc90014131a73e14c99829be98e7f918
Cédric Bonhomme <cedric@cedricbonhomme.org>
2019-08-19 15:25:26 +0200

    Retrieve the MONARC version and latest commit with the GitHub token.

--- build_vm.sh
+++ build_vm.sh
@@ -3,11 +3,13 @@
 # Timing creation
 TIME_START=$(date +%s)
 
+AUTH="Authorization: token $GITHUB_AUTH_TOKEN"
+
 # Latest version of MONARC
 echo "Retrieving information about latest MONARC release..."
-export MONARC_VERSION=$(curl --silent -H 'Content-Type: application/json' https://api.github.com/repos/monarc-project/MonarcAppFO/releases/latest | jq  -r '.tag_name')
+export MONARC_VERSION=$(curl --silent -sH "$AUTH" -H 'Content-Type: application/json' https://api.github.com/repos/monarc-project/MonarcAppFO/releases/latest | jq  -r '.tag_name')
 # Latest commit hash of MONARC
-export LATEST_COMMIT=$(curl --silent -H 'Content-Type: application/json' -s https://api.github.com/repos/monarc-project/MonarcAppFO/commits  | jq -r '.[0] | .sha')
+export LATEST_COMMIT=$(curl --silent -sH "$AUTH" -H 'Content-Type: application/json' -s https://api.github.com/repos/monarc-project/MonarcAppFO/commits  | jq -r '.[0] | .sha')
 # Fetching latest MONARC LICENSE
 /usr/bin/wget -q -O /tmp/LICENSE-MONARC https://raw.githubusercontent.com/monarc-project/MonarcAppFO/master/LICENSE
 # Enable logging for packer
