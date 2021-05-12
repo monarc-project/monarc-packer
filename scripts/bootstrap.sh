a5d11adefaabfa7627f50a6b20cf0b1f500488e3
Cédric Bonhomme <cedric@cedricbonhomme.org>
2021-05-12 22:23:27 +0200

    fix

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -94,7 +94,7 @@ sudo apt-get update
 
 echo "--- Installing PHP-specific packages… ---"
 sudo apt-get -y install php7.4 apache2 libapache2-mod-php php7.4-curl php7.4-gd php7.4-mysql php7.4-apcu php7.4-xml php7.4-mbstring php7.4-intl php7.4-imagick php7.4-zip php7.4-bcmath > /dev/null
-#sudo apt-get -y remove php8.0-cli
+sudo apt-get -y remove php8.0-cli
 
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 php -r "if (hash_file('sha384', 'composer-setup.php') === '756890a4488ce9024fc62c56153228907f1545c228516cbf63f885e036d37e9a59d27d63f46af1d4d07ee0f76181c7d3') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
