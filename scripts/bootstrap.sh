4110e6495718b75296dc1256263516b3ff3f0937
Ruslan Baidan <ruslan.baidan@securitymadein.lu>
2021-05-12 22:22:15 +0200

    Php7.4 version install fixes.

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -88,13 +88,12 @@ EOF
 sudo apt-get purge -y expect > /dev/null 2>&1
 
 
+echo "--- Installing PHP-specific packages… ---"
 sudo apt -y install software-properties-common
 sudo add-apt-repository ppa:ondrej/php
 sudo apt-get update
-
-echo "--- Installing PHP-specific packages… ---"
-sudo apt-get -y install php7.4 apache2 libapache2-mod-php php7.4-curl php7.4-gd php7.4-mysql php7.4-apcu php7.4-xml php7.4-mbstring php7.4-intl php7.4-imagick php7.4-zip php7.4-bcmath > /dev/null
-#sudo apt-get -y remove php8.0-cli
+sudo apt -y install php7.4
+sudo apt-get -y install apache2 libapache2-mod-php php7.4-curl php7.4-gd php7.4-mysql php-apcu php-xml php7.4-mbstring php7.4-intl php-imagick php7.4-zip php7.4-bcmath > /dev/null
 
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 php -r "if (hash_file('sha384', 'composer-setup.php') === '756890a4488ce9024fc62c56153228907f1545c228516cbf63f885e036d37e9a59d27d63f46af1d4d07ee0f76181c7d3') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
