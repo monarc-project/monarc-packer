c185f72b6755626face009e350bda079ecccedaa
Cédric Bonhomme <cedric@cedricbonhomme.org>
2021-05-17 19:56:47 +0200

    install composer in /usr/bin

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -99,7 +99,7 @@ sudo apt-get -y remove php8.0-cli
 
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 php -r "if (hash_file('sha384', 'composer-setup.php') === '756890a4488ce9024fc62c56153228907f1545c228516cbf63f885e036d37e9a59d27d63f46af1d4d07ee0f76181c7d3') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
-php composer-setup.php --install-dir=/tmp --filename=composer
+php composer-setup.php --install-dir=/usr/bin --filename=composer
 php -r "unlink('composer-setup.php');"
 mv /tmp/composer /usr/local/bin/composer
 
