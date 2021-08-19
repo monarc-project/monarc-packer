59954b12b0f97d21a3ab219ebaf8b40af7935ddc
Benjamin Schmitz <66966223+bensofficial@users.noreply.github.com>
2021-08-19 16:31:48 +0200

    Add root privileges

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -43,7 +43,7 @@ export LANGUAGE=en_US.UTF-8
 export LANG=en_US.UTF-8
 export LC_ALL=en_US.UTF-8
 locale-gen en_US.UTF-8
-dpkg-reconfigure locales
+sudo dpkg-reconfigure locales
 
 echo "--- Installing MONARC FO… ---"
 
